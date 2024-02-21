52fd3d48f4ed9cd6c01c949520afed0a4e7bd5ae
Space Pocket <space1985pocket@gmail.com>
2024-02-21 17:12:11 +0300

    feat: add  CartIcon

---
+++ src/components/Layout/HeaderCartButton.tsx
@@ -0,0 +1,11 @@
+import styles from './HeaderCartButton.module.css';
+
+export const HeaderCartButton = (): JSX.Element => {
+  return (
+    <button>
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  );
+};
